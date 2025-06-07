08b519848520a87f15551bcf5cddbc1811eeef1a
Yunus Çetin <93588469+lordcetin@users.noreply.github.com>
2025-06-07 11:14:54 +0300

    testr

--- lib/solvePass.ts
+++ lib/solvePass.ts
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 /* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use server'
-// const bcrypt = require("bcryptjs");
-import bcrypt from 'bcrypt';
+const bcrypt = require("bcryptjs");
+// import bcrypt from 'bcryptjs';
 
 export const saltAndHashPassword = async (password:any,hashedPass:any) => {
   let solve = await bcrypt.compare(
